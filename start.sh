353ca166f4efada810329e883dca08cbd46ebb3b
hyperhhah <131239959+hyperhhah@users.noreply.github.com>
2023-05-04 18:34:29 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Joelkb/DQ-the-file-donor.git /DQTheFileDonorBot
+  git clone https://github.com/hyperhhah/autofilter.git /autofilter
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /DQTheFileDonorBot
+  git clone $UPSTREAM_REPO /autofilter
 fi
-cd /DQTheFileDonorBot
+cd /autofilter
 pip3 install -U -r requirements.txt
-echo "Starting DQ-The-File-Donor...."
+echo "Starting Sana-Maria...."
 python3 bot.py
